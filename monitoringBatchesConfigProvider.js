9c1bb5e367d8b78f6b06954b434453b3264c882b
Kalidas Muthusamy <16206463+kalidasmuthusamy@users.noreply.github.com>
2021-08-26 18:02:25 +0530

    Revert "Remove Optional Seconds Expression"
    This reverts commit 6b4c9fc93444e131cd2170677f947b8f3d5daeae.

--- monitoringBatchesConfigProvider.js
+++ monitoringBatchesConfigProvider.js
@@ -8,14 +8,7 @@ const monitoringBatchesConfig = [
       proxyRestricted: [],
     },
     recipientEmailAddresses: ["bar@example.com", "fab@example.com"],
-<<<<<<< Updated upstream
     cronSchedule: "*/10 * * * * *",
-=======
-<<<<<<< Updated upstream
-=======
-    cronSchedule: "* * * * *",
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   },
   {
     urls: {
@@ -27,14 +20,7 @@ const monitoringBatchesConfig = [
       proxyRestricted: [],
     },
     recipientEmailAddresses: ["damn@example.com", "bam@example.com"],
-<<<<<<< Updated upstream
     cronSchedule: "*/20 * * * * *",
-=======
-<<<<<<< Updated upstream
-=======
-    cronSchedule: "* * * * *",
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   },
 ];
 
