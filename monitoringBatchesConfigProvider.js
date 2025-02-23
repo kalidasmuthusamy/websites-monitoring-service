86e67c4789522ba4684ac7890fedd297d83a29da
Kalidas Muthusamy <16206463+kalidasmuthusamy@users.noreply.github.com>
2025-02-23 12:50:49 +0530

    use vcw sites

--- monitoringBatchesConfigProvider.js
+++ monitoringBatchesConfigProvider.js
@@ -2,26 +2,25 @@ const monitoringBatchesConfig = [
   {
     urls: {
       public: [
-        "https://portal.uiic.in/GCWebPortal/login/LoginAction.do?p=login",
-        "https://portal.uiic.in/GCWebPort/login/LoginAction.do?p=login",
+        "https://www.vcw.ac.in/",
+        "https://www.vcw.ac.in/degree-exam-applications/",
+        "https://www.vcw.ac.in/self-learning-exam-application/",
       ],
       proxyRestricted: [],
     },
-    recipientEmailAddresses: ["bar@example.com", "fab@example.com"],
-    cronSchedule: "* * * * *",
+    recipientEmailAddresses: ["admin@vcw.ac.in", "management@vcw.ac.in"],
+    cronSchedule: "*/1 * * * *",
     emailSubject: "Batch 1",
   },
   {
     urls: {
       public: [
-        "https://github.com",
-        "https://npmjs.com",
-        "https://portal.uiic.in/GCWebPort/login/LoginAction.do?p=login",
+        "https://pay.velalarengg.ac.in/pay/",
       ],
       proxyRestricted: [],
     },
-    recipientEmailAddresses: ["damn@example.com", "bam@example.com"],
-    cronSchedule: "* * * * *",
+    recipientEmailAddresses: ["fees@vcw.ac.in", "management@vcw.ac.in"],
+    cronSchedule: "*/1 * * * *",
     emailSubject: "Batch 2",
   },
 ];
